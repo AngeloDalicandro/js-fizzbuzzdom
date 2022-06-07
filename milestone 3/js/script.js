bdad68ddb8dd7c29d814e17440ad707d8c8cfd7f
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-06-07 15:29:02 +0200

    completed

--- milestone 3/js/script.js
+++ milestone 3/js/script.js
@@ -3,18 +3,23 @@ let fizzBuzzDom;
 
 for( i = 1; i <= 100; i++) {
     // Creo le li in js
-    let list = document.querySelector('ul');
+    let list = document.getElementById('list');
+    let listItem = document.createElement('li');
+    listItem.classList.add("box");
 
     // Seleziono i multipli di 5 e 3 e stampo nelle li
     if( (i % 5) === 0) {
         fizzBuzzDom = 'buzz';
+        listItem.classList.add('buzz')
     } else if( (i % 3) === 0) {
         fizzBuzzDom = 'fizz';
+        listItem.classList.add('fizz')
     } else {
         fizzBuzzDom = i;
     }
 
-    const listItem = `<li> ${fizzBuzzDom} </li> `
-    list.innerHTML += listItem;
+    listItem.append( ` ${fizzBuzzDom}  ` )
 
+    console.log( listItem )
+    list.append( listItem )
     }
